f6361aff464b08320fd549da2de883bf22e348c9
Gabriela de Andrade <166560478+gabrieladeandradecode@users.noreply.github.com>
2026-02-23 17:11:00 -0300

    Aula 11 - Criando um menu hambúrguer

---
+++ exercicios/ex026/mq005/script.js
@@ -0,0 +1,15 @@
+ let itens = document.getElementById("itensMenu");
+
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none';
+    } else {
+        itens.style.display = 'block';
+    }
+}
+
+
+
+// function clickMenu() {
+//     itens.classList.toggle("ativo");
+// }
