9b6a56658b507392e60672951ab6e84b33e150e8
Gabriela de Andrade <166560478+gabrieladeandradecode@users.noreply.github.com>
2026-02-24 10:11:36 -0300

    Criação de media query para exercício 5 - onresize js

--- exercicios/ex026/mq005/script.js
+++ exercicios/ex026/mq005/script.js
@@ -1,6 +1,6 @@
  let itens = document.getElementById("itensMenu");
 
-function clickMenu() {
+function clicarMenu() {
     if (itens.style.display == 'block') {
         itens.style.display = 'none';
     } else {
@@ -8,6 +8,13 @@ function clickMenu() {
     }
 }
 
+function alterarTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block';
+    } else {
+        itens.style.display = 'none';
+    }
+}
 
 
 // function clickMenu() {
